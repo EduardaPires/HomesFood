4150781ffc53a21bbefc1cb00357a748695a772f
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-10-26 23:56:20 -0300

    modificação registro

--- Homes food project/src/Registro.java
+++ Homes food project/src/Registro.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class Registro {
-    private ArrayList listaClientes;
-    private ArrayList listaCozinheiros;
+    private ArrayList<User> listaClientes;
+    private ArrayList<User> listaCozinheiros;
     
 
     public Registro(){
