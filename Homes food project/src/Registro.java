a4ef42706f9908576f63a5a85a71a20c164203fd
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-10-26 23:53:08 -0300

    Alterações na classe registro

--- Homes food project/src/Registro.java
+++ Homes food project/src/Registro.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
 public class Registro {
+    private ArrayList listaClientes;
+    private ArrayList listaCozinheiros;
     
+
+    public Registro(){
+        this.listaClientes = new ArrayList<>();
+        this.listaCozinheiros = new ArrayList<>();
+    }
+
+    public void registrar(User usuario){
+        
+    }
+
+    public void apagarUsuario(){
+
+    }
+
+    public User procurar(User usuario){
+        return null;
+    }
+
 }
